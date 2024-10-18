834046e2130079d10270c46def690383b1209806
EleSoyEle <angelgvsgm@gmail.com>
2024-10-17 18:53:11 -0600

    Arreglos en las series

--- animation.py
+++ animation.py
@@ -1,19 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from utils import calc_cnm
+from utils import calc_cnm,calc_sin_cos,calc_cos_t
 
 #Numero de iteraciones maximas en las suams
 n = 10
 m = 10
 #Definimos el contorno
-l1 = 1
-l2 = 1
-v = 1
+l1 = 10
+l2 = 10
+v = 50
 #Definimos las iteraciones totales de la simulacion
 epochs = 1000
 
+t0 = 0
+t1 = 1
 
-f1 = lambda x,y: np.exp((-x**2-y**2)/0.1)
-c = calc_cnm(n,m,)
 
+def f1(x,y):
+    return 10*np.exp(-((x-5)**2+(y-5)**2)/0.1)
+
+res = 200
+x0 = np.linspace(0,l1,res)
+x1 = np.linspace(0,l2,res)
+X,Y = np.meshgrid(x0,x1)
+time_t = np.linspace(t0,t1,epochs)
+
+
+c = calc_cnm(n,m,f1,l1,l2)
+
+def calc_frames():
+    frames = []
+    s1,s2 = calc_sin_cos(n,m,l1,l2,X,Y)
+    for epoch in range(epochs):
+        ct = calc_cos_t(n,m,v,time_t[epoch],l1,l2)
+        u = np.zeros((res,res))
+        for i in range(n):
+            for j in range(m):
+                u += c[i,j]*s1[i]*s2[j]*ct[i,j]
+        frames.append(u)
+    return frames
+
+
+frames = calc_frames()
+
+fig = plt.figure(figsize=(6,6))
+ax = fig.add_subplot()
+
+def animate(i):
+    print(i)
+    ax.clear()
+    ax.pcolormesh(X,Y,frames[i],vmin=np.min(frames),vmax=np.max(frames),cmap='PuBu_r')
+    ax.axis("off")
+ani = FuncAnimation(fig,animate,epochs,interval=0.1)
+plt.show()    
--- utils.py
+++ utils.py
@@ -4,12 +4,30 @@ import scipy
 
 #Para el calculo de los coeficientes de Fourier
 def calc_cnm(n,m,f,l1,l2):
-    c = np.zeros(n,m)
-    freq1 = np.pi*n/l1
-    freq2 = np.pi*m/l2
-    fi = lambda x,y: f(x,y)*np.sin(freq1*x)*np.sin(freq2*y)
+    c = np.zeros(shape=[n,m])
     for i in range(n):
         for j in range(m):
-            c[i,j] = dblquad(fi,0,l1,0,l2)
+            freq1 = np.pi*(i+1)/l1
+            freq2 = np.pi*(j+1)/l2
+            fi = lambda x,y: f(x,y)*np.sin(freq1*x)*np.sin(freq2*y)
+            integ = dblquad(fi,0,l1,0,l2)
+            c[i,j] = 4*integ[0]/(l1*l2)
     return c
 
+def calc_sin_cos(n,m,l1,l2,x,y):
+    s1 = []
+    s2 = []
+    for i in range(max(n,m)):
+        tq = np.pi*(i+1)
+        s1.append(np.sin(tq*x/l1))
+        s2.append(np.sin(tq*y/l2))
+    return s1,s2
+
+
+def calc_cos_t(n,m,v,t,l1,l2):
+    sp = np.zeros((n,m))
+    for i in range(n):
+        for j in range(m):
+            sp[i,j] = np.cos(np.pi*v*np.sqrt(pow((i+1)/l1,2)+pow((j+1)/l2,2))*t)
+
+    return sp
